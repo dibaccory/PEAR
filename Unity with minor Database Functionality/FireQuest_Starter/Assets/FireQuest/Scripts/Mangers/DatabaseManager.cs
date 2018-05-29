bd6d8f6b37c8c9b2c4b4ef0b7edbf61d375fea3f
kuzeal <35326679+kuzeal@users.noreply.github.com>
2018-05-29 19:48:29 -0400

    Unity Project Upload

---
+++ Unity with minor Database Functionality/FireQuest_Starter/Assets/FireQuest/Scripts/Mangers/DatabaseManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class DatabaseManager : MonoBehaviour {
+
+	public static DatabaseManager sharedInstance = null;
+
+	/// <summary>
+	/// Awake this instance and initialize sharedInstance for Singleton pattern
+	/// </summary>
+	void Awake() {
+		if (sharedInstance == null) {
+			sharedInstance = this;
+		} else if (sharedInstance != this) {
+			Destroy (gameObject);  
+		}
+
+		DontDestroyOnLoad(gameObject);
+
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://pear-9120e.firebaseio.com/");
+
+        Debug.Log(Router.Users());
+	}
+	
+    public void CreateNewUser(User user, string uid)
+    {
+        string userJSON = JsonUtility.ToJson(user);
+        Router.UserWithUID(uid).SetRawJsonValueAsync(userJSON);
+    }
+}
---
+++ Unity with minor Database Functionality/FireQuest_Starter/Assets/FireQuest/Scripts/Utilities/Router.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class Router : MonoBehaviour {
+
+    private static DatabaseReference baseRef = FirebaseDatabase.DefaultInstance.RootReference;
+
+    public static DatabaseReference Users()
+    {
+        return baseRef.Child("users");
+    }
+
+    public static DatabaseReference UserWithUID(string uid)
+    {
+        return baseRef.Child("users").Child(uid);
+    }
+
+}
