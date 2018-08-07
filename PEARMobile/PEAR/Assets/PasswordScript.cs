6adb6c3cd7cec809441c55ad82640ea9799204f2
Matthew Frazier <frazier85@gmail.com>
2018-08-07 17:51:44 -0400

    Added a login screen. UI only.

---
+++ PEARMobile/PEAR/Assets/PasswordScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordScript : MonoBehaviour {
+    public InputField password;
+	// Use this for initialization
+	void Start () {
+        password.contentType = InputField.ContentType.Password;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
