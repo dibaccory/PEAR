dd39eb41171417db4073f7e5e1cb2b277e0e6e65
Matthew Frazier <frazier85@gmail.com>
2018-08-14 12:08:02 -0400

    Added Firebase SDK to PEAR Mobile. Started work on user authentication. Not working properly yet. Clicking on login or sign up buttons loads Vuforia Test Scene

---
+++ PEARMobile/PEAR/Assets/Scripts/managers/AuthManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using System.Threading.Tasks;
+
+public class AuthManager : MonoBehaviour
+{
+
+    Firebase.Auth.FirebaseAuth auth;
+
+    // Delegates
+    public delegate IEnumerator AuthCallback(Task<Firebase.Auth.FirebaseUser> task, string operation);
+    public event AuthCallback authCallback;
+
+    private void Awake()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+    }
+
+    public void SignUpNewUser(string email, string password)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            StartCoroutine(authCallback(task, "sign_up"));
+        });
+    }
+
+    public void LoginExistingUser(string email, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            StartCoroutine(authCallback(task, "login"));
+        });
+    }
+}
+
+//import UIKit
+//import Firebase
+
+//@UIApplicationMain
+//class AppDelegate : UIResponder, UIApplicationDelegate
+//{
+
+//    var window: UIWindow?
+
+//    func application(_ application: UIApplication,
+//      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+//    -> Bool {
+//    FirebaseApp.configure()
+//    return true
+//  }
+//}
---
+++ PEARMobile/PEAR/Assets/Scripts/managers/FormManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine.SceneManagement;
+
+public class FormManager : MonoBehaviour
+{
+
+    // UI objects linked from the inspector
+    public InputField emailInput;
+    public InputField passwordInput;
+
+    public Button signUpButton;
+    public Button loginButton;
+
+    public Text statusText;
+
+    public AuthManager authManager;
+
+    void Awake()
+    {
+        ToggleButtonStates(false);
+        // Auth delegate subscriptions
+        authManager.authCallback += HandleAuthCallback;
+    }
+
+    /// <summary>
+    /// Validates the email input
+    /// </summary>
+    public void ValidateEmail()
+    {
+        string email = emailInput.text;
+        var regexPattern = @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+                + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+                + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$";
+
+        if (email != "" && Regex.IsMatch(email, regexPattern))
+        {
+            ToggleButtonStates(true);
+        }
+        else
+        {
+            ToggleButtonStates(false);
+        }
+    }
+
+    // Firebase methods
+    public void OnSignUp()
+    {
+        authManager.SignUpNewUser(emailInput.text, passwordInput.text);
+        Debug.Log("Sign Up");
+    }
+
+    public void OnLogin()
+    {
+        authManager.LoginExistingUser(emailInput.text, passwordInput.text);
+        Debug.Log("Login");
+    }
+
+    IEnumerator HandleAuthCallback(Task<Firebase.Auth.FirebaseUser> task, string operation)
+    {
+        //if (task.IsFaulted || task.IsCanceled)
+        //{
+        //    UpdateStatus("sorry, error! Error: " + task.Exception);
+        //}
+        //else if (task.IsCompleted)
+        //{
+            // Firebase.Auth.FirebaseUser newPlayer = task.Result;
+            UpdateStatus("Loading the game scene");
+
+            yield return new WaitForSeconds(1.5f);
+            SceneManager.LoadScene("VuforiaTesting");
+        //}
+    }
+
+    void OnDestroy()
+    {
+        authManager.authCallback -= HandleAuthCallback;
+    }
+
+    // Utilities
+    void ToggleButtonStates(bool toState)
+    {
+        signUpButton.interactable = toState;
+        loginButton.interactable = toState;
+    }
+
+    void UpdateStatus(string message)
+    {
+        statusText.text = message;
+    }
+}
