1f2240ab7b6bd90a5291954613a7ee3527dba638
Matthew Frazier <frazier85@gmail.com>
2018-08-23 13:20:33 -0400

    Beginning work on almanac inventory system. Got a basic strcture so far from a tutorial.

---
+++ PEARMobile/PEAR/Assets/Scripts/almanac/AlmanacEditor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(Almanac))]
+public class AlmanacEditor : Editor {
+
+    private SerializedProperty itemImagesProperty;
+    private SerializedProperty itemsProperty;
+
+    private bool[] showItemSlots = new bool[Almanac.numItemSlots];
+
+    private const string almanacPropItemImagesName = "itemImages";
+    private const string almanacPropItemsName = "items";
+
+    private void OnEnable()
+    {
+        itemImagesProperty = serializedObject.FindProperty(almanacPropItemImagesName);
+        itemsProperty = serializedObject.FindProperty(almanacPropItemsName);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        for (int i = 0; i < Almanac.numItemSlots; i++)
+        {
+            ItemSlotGUI(i);
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void ItemSlotGUI(int index)
+    {
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+        EditorGUI.indentLevel++;
+
+        showItemSlots[index] = EditorGUILayout.Foldout(showItemSlots[index], "Item slot " + index);
+
+        if(showItemSlots[index])
+        {
+            EditorGUILayout.PropertyField(itemImagesProperty.GetArrayElementAtIndex(index));
+            EditorGUILayout.PropertyField(itemsProperty.GetArrayElementAtIndex(index));
+
+        }
+
+        EditorGUI.indentLevel--;
+        EditorGUILayout.EndVertical();
+    }
+}
