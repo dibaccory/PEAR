e9e5dde1165db0fbff35f37c28473e8b3a5ca789
Matthew Frazier <frazier85@gmail.com>
2018-08-29 15:37:08 -0400

    Some work on the item viewing scene. Added the video player. Also added the vuforia license to the project... but I don't think we'll need it. Going to merge this branch now and create a new one for more item veiwing work

--- PEARMobile/PEAR/Assets/Scripts/almanac/Almanac.cs
+++ PEARMobile/PEAR/Assets/Scripts/almanac/Almanac.cs
@@ -62,37 +62,4 @@ public class Almanac : MonoBehaviour {
         instance = this;
         UpdatePanelSlots();
     }
-
-    //public const int numItemSlots = 9;
-
-    //public Image[] itemImages = new Image[numItemSlots];
-    //public Item[] items = new Item[numItemSlots];
-
-    //public void AddItem(Item itemToAdd) 
-    //{
-    //    for (int i = 0; i < items.Length; i++) 
-    //    {
-    //        if(items[i] == null)
-    //        {
-    //            items[i] = itemToAdd;
-    //            itemImages[i].sprite = itemToAdd.sprite;
-    //            itemImages[i].enabled = true;
-    //            return;
-    //        }
-    //    }
-    //}
-
-    //public void RemoveItem(Item itemToRemove)
-    //{
-    //    for (int i = 0; i < items.Length; i++)
-    //    {
-    //        if (items[i] == itemToRemove)
-    //        {
-    //            items[i] = null;
-    //            itemImages[i].sprite = null;
-    //            itemImages[i].enabled = false;
-    //            return;
-    //        }
-    //    }
-    //}
 }
