ff3045cec0f4bf1ddf6ca7e5aea02ab709d1ed80
Richard <bacchus@Richards-MacBook-Pro.local>
2018-09-02 15:00:58 -0400

    Camera gyroscope working; materials randomly populate the screen at runtime

---
+++ PEARMobile/PEAR/Assets/Scripts/utilities/init_orb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class init_orb : MonoBehaviour {
+
+	int count = 10; //Should get this from the database; i.e. How many things do we need to collect for this module
+
+	public GameObject orb;
+	// Use this for initialization
+	void Start () {
+		for(int i=0; i < count; i++) {
+			orb = Resources.Load("Orb") as GameObject;
+			Vector3 position = Random.onUnitSphere*30;
+			if(position.y < -10) { //Ensure user doesn't have to look to low
+				position.y = Mathf.Abs(position.y);
+			}
+			Instantiate(orb, position, Quaternion.identity);
+			//Orb needs to have a script to check if the user had interacted with it.
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
