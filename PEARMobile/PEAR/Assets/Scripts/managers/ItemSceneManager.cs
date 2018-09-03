26b4de84b5741ad0525c38c7e4812a81a776b439
Matthew Frazier <frazier85@gmail.com>
2018-09-03 13:49:56 -0400

    Users can now toggle between questions and video on item viewing scene

---
+++ PEARMobile/PEAR/Assets/Scripts/managers/ItemSceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSceneManager : MonoBehaviour {
+
+    public Button goToQuestionsButton;
+    public Button goToVideoButton;
+    public GameObject videoQuad;
+    public GameObject questionsQuad;
+
+    private void Awake()
+    {
+        ToggleVideo(true);
+    }
+
+    public void OnQuestionButtonClick()
+    {
+        ToggleVideo(false);
+    }
+
+    public void OnVideoButtonClick()
+    {
+        ToggleVideo(true);
+    }
+
+    private void ToggleVideo(bool value)
+    {
+        videoQuad.SetActive(value);
+        questionsQuad.SetActive(!value);
+    }
+
+}
