4e08d360cef4b4b2482794867490a230b612d98b
Matthew Frazier <frazier85@gmail.com>
2018-09-18 14:25:57 -0400

    Adding question class

---
+++ PEARMobile/PEAR/Assets/Scripts/item/Question.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour {
+
+    private string mQuestion;
+    string correctAnswer;
+    private List<string> otherAnswers;
+
+    public string MQuestion
+    {
+        get
+        {
+            return mQuestion;
+        }
+
+        set
+        {
+            mQuestion = value;
+        }
+    }
+
+    public List<string> MAnswers
+    {
+        get
+        {
+            return mAnswers;
+        }
+
+        set
+        {
+            mAnswers = value;
+        }
+    }
+}
