ee2ff9d0fd89ca1b20e24d34f132cc54d1e79ae7
Kenia Castro <k.castro@knights.ucf.edu>
2018-09-28 22:53:51 -0400

    updated firebase, anuglar, added sidenav

--- PEARWebsite/PEAR-app/src/app/app.component.ts
+++ PEARWebsite/PEAR-app/src/app/app.component.ts
@@ -6,22 +6,14 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // template: `
-  // <h1>hi</h1>
-  // <ul>
-  //   <li *ngFor="let item of items | async">
-  //     {{ item.name }}
-  //   </li>
-  // </ul>
-  // `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'PEAR';
-  items: Observable<any>;
-  constructor(db: AngularFireDatabase) {
-    this.items = db.object('items').valueChanges();
-  }
+  // items: Observable<any>;
+  // constructor(db: AngularFireDatabase) {
+  //   this.items = db.object('items').valueChanges();
+  // }
 
   // writeUserData(db: AngularFireDatabase, userId, name, email) {
   //   firebase.database().ref('users/' + userId).set({
@@ -29,7 +21,7 @@ export class AppComponent {
   //     email: email
   //   });
   // }
-  
+
   // readUserData(db: AngularFireDatabase) {
   //   db.database.ref('/users/' + "Cqgkn1hpngSOM01Q859LMg36gBE2");
     // .once('value').then(function(snapshot) {
--- PEARWebsite/PEAR-app/src/app/user.ts
+++
@@ -1,6 +0,0 @@
-export class Admin {
-    id: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-}
