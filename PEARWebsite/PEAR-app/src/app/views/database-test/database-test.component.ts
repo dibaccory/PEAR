32f44a6f2677334f570f7897f4ccc0c7cb846afa
Kenia Castro <k.castro@knights.ucf.edu>
2018-09-29 01:56:00 -0400

    access database values in console.log

--- PEARWebsite/PEAR-app/src/app/views/database-test/database-test.component.ts
+++ PEARWebsite/PEAR-app/src/app/views/database-test/database-test.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-database-test',
+//   template: `
+//   <ul>
+//   <li **ngFor="let item of items | async">
+//     {{ item | json }}
+//   </li>
+// </ul>
+// `,
   templateUrl: './database-test.component.html',
   styleUrls: ['./database-test.component.css']
 })
 export class DatabaseTestComponent implements OnInit {
+  items: Observable<any>;
 
-  constructor() { }
+  constructor(db: AngularFireDatabase) {
+    // this.items = db.list('answers').valueChanges();
+    db.list<any>('users').valueChanges().subscribe(console.log);
+  }
 
   ngOnInit() {
   }
