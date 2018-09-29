56b5fc157973efecb19c5b7b40e4ad4b664ccb41
Richard <thedibaccle@gmail.com>
2018-09-29 12:51:43 -0400

    Adding in quick changes for Router / camera

--- PEARMobile/PEAR/Assets/Scripts/utilities/Router.cs
+++ PEARMobile/PEAR/Assets/Scripts/utilities/Router.cs
@@ -55,4 +55,25 @@ public class Router : MonoBehaviour
         return baseRef.Child("classrooms").Child(classCode).Child("modules").Child(moduleName).Child(item).Child(buildOrCollect);
     }
 
+
+
+    public static DatabaseReference GetModules (string classCode)
+    {
+      return baseRef.Child("classrooms").Child(classCode).Child("modules");
+    }
+
+    //Get all the Materials for current module
+    public static DatabaseReference ModuleMaterials (string classCode, string moduleName)
+    {
+      //Do we have a getCurrentModule function?
+        return baseRef.Child("classrooms").Child(classCode).Child("modules").Child("solar system").Children();
+    }
+
+    /*
+    TODO Get current Module (to make this generic)
+    pull in master
+
+    */
+
+
 }
