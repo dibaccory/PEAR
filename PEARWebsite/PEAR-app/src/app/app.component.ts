59fbc623c9b51bd6b80324897cee5cf230b62209
Kenia Castro <k.castro@knights.ucf.edu>
2018-10-12 10:23:06 -0400

    minor changes

--- PEARWebsite/PEAR-app/src/app/app.component.ts
+++ PEARWebsite/PEAR-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 // import { AngularFireDatabase } from '@angular/fire/database';
 // import { AngularFireAuth } from '@angular/fire/auth';
 // import { auth } from 'firebase/app';
@@ -12,4 +14,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'PEAR';
   loggedIn = false;
+  // user = this.afAuth.auth.currentUser;
+
+  // user:
+  // {
+  //   email: string,
+  //   password: string
+  // };
+
+  // constructor(public afAuth: AngularFireAuth) {
+  //   this.afAuth.auth.onAuthStateChanged(function(user) {
+  //     if (user) {
+  //       // User is signed in.
+  //       // const loggedIn = true;
+  //     } else {
+  //       // No user is signed in.
+  //       // const loggedIn = false;
+  //     }
+  //   });
+  // }
 }
