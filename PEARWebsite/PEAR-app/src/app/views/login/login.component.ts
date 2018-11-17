7542e765e5a53153ef3bd4d7ab8303bf1926e150
Kenia Castro <k.castro@knights.ucf.edu>
2018-11-17 02:58:25 -0500

    checks if email/pw is registered student or teacher

--- PEARWebsite/PEAR-app/src/app/views/login/login.component.ts
+++ PEARWebsite/PEAR-app/src/app/views/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import * as admin from 'firebase-admin';
 
 @Component({
   selector: 'app-login',
@@ -10,25 +12,57 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  user = {
-    email: '',
-    password: ''
-  };
+  module;
+  UID: string;
+  key;
+  isATeacher = false;
+  teacherUIDs: string[] = [];
 
-  constructor(public afAuth: AngularFireAuth, private router: Router) {
+  email;
+  password;
+
+  constructor(public afAuth: AngularFireAuth, public db: AngularFireDatabase, private router: Router) {
+    this.module = this.db.database.ref('teachers').orderByChild('uid');
+
+    this.module.once('value')
+      .then((snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+          this.key = childSnapshot.key; // UID
+          this.teacherUIDs.push(this.key.toString());
+
+          console.log('uids ' + this.teacherUIDs);
+        });
+      });
   }
 
-login() {
-  this.afAuth.auth.signInWithEmailAndPassword(this.user.email, this.user.password)
+  login() {
+    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password)
       .then((res) => {
-        console.log(res);
-        this.router.navigate(['/dashboard']);
+        this.UID = res.user.uid;
+        this.checkIfTeacher();
       })
       .catch((err) => {
         console.log('error: ' + err);
         alert('Wrong email or password.');
         this.router.navigate(['/login']);
       });
+  }
+
+  checkIfTeacher() {
+    for (let uid of this.teacherUIDs) {
+      if (this.UID === uid) {
+        this.isATeacher = true;
+      }
+    }
+
+    console.log('isATeacher ' + this.isATeacher);
+
+    if (this.isATeacher === true) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      alert('This is not a registered teacher account');
+      this.router.navigate(['/login']);
+    }
   }
 
   ngOnInit() {
